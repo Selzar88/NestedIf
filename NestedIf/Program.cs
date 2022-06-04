9b7d1307b2e4d285dc9ab7a84ed9388b3c08256c
wisni <wisni@LAPTOP-7AN5K9RK>
2022-06-04 14:30:59 +0100

    Add project files.

---
+++ NestedIf/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NestedIf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isAdmin = false;
+            bool isRegistered = true;
+            string userName = "";
+            Console.WriteLine("Enter your username");
+            
+            
+            userName = Console.ReadLine();
+            if (isRegistered && userName != "" && userName.Equals("admin"))
+            {
+                Console.WriteLine("Hi there, registered user");
+
+
+                Console.WriteLine("Hi there, " + userName);
+
+                Console.WriteLine("Hi there, Admin");
+            }
+                }
+            }
+
+ 
+        }
+    
